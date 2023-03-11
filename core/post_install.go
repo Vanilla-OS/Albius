c15100169a5bbc07805ffbfd5f9baa87eea460fc
Mateus Melchiades <mateusbmelchiades@gmail.com>
2023-03-11 09:09:07 -0300

    post_install: Timezone

---
+++ core/post_install.go
@@ -0,0 +1,21 @@
+package albius
+
+import (
+	"fmt"
+	"os"
+)
+
+// Set locale
+// Set keyboard
+// Set user?
+
+func SetTimezone(tz string, targetRoot string) error {
+	tzPath := targetRoot + "/etc/timezone"
+
+	err := os.WriteFile(tzPath, []byte(tz), 0644)
+	if err != nil {
+		return fmt.Errorf("Failed to set timezone: %s", err)
+	}
+
+	return nil
+}

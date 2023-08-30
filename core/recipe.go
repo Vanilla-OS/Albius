24f83ad1361838579ea7b4e0bad9edb9067f1b88
Mateus Melchiades <matbme@duck.com>
2023-08-29 22:09:24 -0300

    Feat: Add recipe documentation

--- core/recipe.go
+++ core/recipe.go
@@ -96,13 +96,32 @@ func runSetupOperation(diskLabel, operation string, args []interface{}) error {
 		return err
 	}
 
+	/* !! ## Setup */
 	switch operation {
+	/* !! ### label
+	 * Creates a new partition table on the disk.
+	 *
+	 * Accepts:
+	 * - Label type (`string`): The partitioning scheme. Either `mbr` or `gpt`.
+	 */
 	case "label":
 		label := DiskLabel(args[0].(string))
 		err = disk.LabelDisk(label)
 		if err != nil {
 			return fmt.Errorf("Failed to execute operation %s: %s", operation, err)
 		}
+	/* !! ### mkpart
+	 * Creates a new partition on the disk.
+	 *
+	 * Accepts:
+	 * - Name (`string`): The name for the partition.
+	 * - FsType (`string`): The filesystem for the partition. Can be either `btrfs`, `ext[2,3,4]`, `linux-swap`, `ntfs`\*, `reiserfs`\*, `udf`\*, or `xfs`\*. If FsType is prefixed with `luks-` (e.g. `luks-btrfs`), the partition will be encrypted using LUKS2.
+	 * - Start (`int`): The start position on disk for the new partition (in MiB).
+	 * - End (`int`): The end position on disk for the new partition (in MiB), or -1 for using all the remaining space.
+	 * - LUKSPassword (optional `string`): The password used to encrypt the partition. Only relevant if `FsType` is prefixed with `luks-`.
+	 *
+	 * \* = Not fully tested. Please create an issue if you encouter problems.
+	 */
 	case "mkpart":
 		name := args[0].(string)
 		fsType := PartitionFs(args[1].(string))
@@ -146,6 +165,12 @@ func runSetupOperation(diskLabel, operation string, args []interface{}) error {
 				return fmt.Errorf("Failed to execute operation %s: %s", operation, err)
 			}
 		}
+	/* !! ### rm
+	 * Deletes a partition from the disk.
+	 *
+	 * **Accepts**:
+	 * - PartNum (`int`): The partition number on disk (e.g. `/dev/sda3` is partition 3).
+	 */
 	case "rm":
 		partNum, err := strconv.Atoi(args[0].(string))
 		if err != nil {
@@ -155,6 +180,13 @@ func runSetupOperation(diskLabel, operation string, args []interface{}) error {
 		if err != nil {
 			return fmt.Errorf("Failed to execute operation %s: %s", operation, err)
 		}
+	/* !! ### resizepart
+	 * Resizes a partition on disk.
+	 *
+	 * **Accepts**:
+	 * - PartNum (`int`): The partition number on disk (e.g. `/dev/sda3` is partition 3).
+	 * - PartNewSize (`int`): The new size in MiB for the partition.
+	 */
 	case "resizepart":
 		partNum, err := strconv.Atoi(args[0].(string))
 		if err != nil {
@@ -168,6 +200,13 @@ func runSetupOperation(diskLabel, operation string, args []interface{}) error {
 		if err != nil {
 			return fmt.Errorf("Failed to execute operation %s: %s", operation, err)
 		}
+	/* !! ### namepart
+	 * Renames the specified partition.
+	 *
+	 * **Accepts**:
+	 * - PartNum (`int`): The partition number on disk (e.g. `/dev/sda3` is partition 3).
+	 * - PartNewName (`string`): The new name for the partition.
+	 */
 	case "namepart":
 		partNum, err := strconv.Atoi(args[0].(string))
 		if err != nil {
@@ -185,6 +224,14 @@ func runSetupOperation(diskLabel, operation string, args []interface{}) error {
 		if err != nil {
 			return fmt.Errorf("Failed to execute operation %s: %s", operation, err)
 		}
+	/* !! ### setflag
+	 * Sets the value of a partition flag, from the flags supported by parted. See parted(8) for the full list.
+	 *
+	 * **Accepts**:
+	 * - PartNum (`int`): The partition number on disk (e.g. `/dev/sda3` is partition 3).
+	 * - FlagName (`string`): The name of the flag.
+	 * - State (`bool`): The value to apply to the flag. Either `true` or `false`.
+	 */
 	case "setflag":
 		partNum, err := strconv.Atoi(args[0].(string))
 		if err != nil {
@@ -194,6 +241,13 @@ func runSetupOperation(diskLabel, operation string, args []interface{}) error {
 		if err != nil {
 			return fmt.Errorf("Failed to execute operation %s: %s", operation, err)
 		}
+	/* !! ### format
+	 * Formats an existing partition to a specified filesystem. This operation will destroy all data.
+	 *
+	 * **Accepts**:
+	 * - PartNum (`int`): The partition number on disk (e.g. `/dev/sda3` is partition 3).
+	 * - FsType (`string`): The filesystem for the partition. Can be either `btrfs`, `ext[2,3,4]`, `linux-swap`, `ntfs`\*, `reiserfs`\*, `udf`\*, or `xfs`\*.
+	 */
 	case "format":
 		partNum, err := strconv.Atoi(args[0].(string))
 		if err != nil {
@@ -205,6 +259,14 @@ func runSetupOperation(diskLabel, operation string, args []interface{}) error {
 		if err != nil {
 			return fmt.Errorf("Failed to execute operation %s: %s", operation, err)
 		}
+	/* !! ### luks-format
+	 * Same as `format` but encrypts the partition with LUKS2.
+	 *
+	 * **Accepts**:
+	 * - PartNum (`int`): The partition number on disk (e.g. `/dev/sda3` is partition 3).
+	 * - FsType (`string`): The filesystem for the partition. Can be either `btrfs`, `ext[2,3,4]`, `linux-swap`, `ntfs`\*, `reiserfs`\*, `udf`\*, or `xfs`\*.
+	 * - Password (`string`): The password used to encrypt the partition.
+	 */
 	case "luks-format":
 		partNum, err := strconv.Atoi(args[0].(string))
 		if err != nil {
@@ -222,6 +284,7 @@ func runSetupOperation(diskLabel, operation string, args []interface{}) error {
 		if err != nil {
 			return err
 		}
+	/* !! --- */
 	default:
 		return fmt.Errorf("Unrecognized operation %s", operation)
 	}
@@ -246,7 +309,17 @@ func runPostInstallOperation(chroot bool, operation string, args []interface{}) 
 		targetRoot = RootA
 	}
 
+	/* !! ## Post-Installation */
 	switch operation {
+	/* !! ### adduser
+	 * Creates a new user.
+	 *
+	 * **Accepts**:
+	 * - Username (`string`): The username of the new user.
+	 * - Fullname (`string`): The full name (display name) of the new user.
+	 * - Groups (`[string]`): A list of groups the new user belongs to (the new user is automatically part of its own group).
+	 * - Password (optional `string`): The password for the user. If not proviced, password login will be disabled.
+	 */
 	case "adduser":
 		username := args[0].(string)
 		fullname := args[1].(string)
@@ -265,12 +338,24 @@ func runPostInstallOperation(chroot bool, operation string, args []interface{}) 
 		if err != nil {
 			return err
 		}
+	/* !! ### timezone
+	 * Sets the timezone.
+	 *
+	 * **Accepts**:
+	 * - TZ (`string`): The timezone code (e.g. `America/Sao_Paulo`).
+	 */
 	case "timezone":
 		tz := args[0].(string)
 		err := SetTimezone(targetRoot, tz)
 		if err != nil {
 			return err
 		}
+	/* !! ### shell
+	 * Runs a shell command.
+	 *
+	 * **Accepts**:
+	 * - Command(s) (`string` or `[string]`): The shell command(s) to execute.
+	 */
 	case "shell":
 		for _, arg := range args {
 			command := arg.(string)
@@ -284,6 +369,13 @@ func runPostInstallOperation(chroot bool, operation string, args []interface{}) 
 				return err
 			}
 		}
+	/* !! ### pkgremove
+	 * Given a file containing a list of packages, uses the specified package manager to remove them.
+	 *
+	 * **Accepts**:
+	 * - PkgRemovePath (`string`): The path containing the list of packages to remove.
+	 * - RemoveCmd (`string`): The package manager command to remove packages (e.g. `apt remove`).
+	 */
 	case "pkgremove":
 		pkgRemovePath := args[0].(string)
 		removeCmd := args[1].(string)
@@ -291,24 +383,50 @@ func runPostInstallOperation(chroot bool, operation string, args []interface{}) 
 		if err != nil {
 			return err
 		}
+	/* !! ### hostname
+	 * Sets the system's hostname.
+	 *
+	 * **Accepts**:
+	 * - NewHostname (`string`): The hostname to set.
+	 */
 	case "hostname":
 		newHostname := args[0].(string)
 		err := ChangeHostname(targetRoot, newHostname)
 		if err != nil {
 			return err
 		}
+	/* !! ### locale
+	 * Sets the system's locale, using `locale-gen` to generate the locale if not present.
+	 *
+	 * **Accepts**:
+	 * - LocaleCode (`string`): The locale code to use. See `/etc/locale.gen` for the full list of locale codes.
+	 */
 	case "locale":
 		localeCode := args[0].(string)
 		err := SetLocale(targetRoot, localeCode)
 		if err != nil {
 			return err
 		}
+	/* !! ### swapon
+	 * Use the provided partition as swap space.
+	 *
+	 * **Accepts**:
+	 * - Partition (`string`): The partition to use as swap.
+	 */
 	case "swapon":
 		partition := args[0].(string)
 		err := Swapon(targetRoot, partition)
 		if err != nil {
 			return err
 		}
+	/* !! ### keyboard
+	 * Sets the system keyboard layout. See `keyboard(5)` for more details.
+	 *
+	 * **Accepts**:
+	 * - Layout (`string`): The keyboard's layout (XKBLAYOUT).
+	 * - Model (`string`): The keyboard's model (XKBMODEL).
+	 * - Variant (`string`): The keyboard's variant (XKBVARIANT).
+	 */
 	case "keyboard":
 		layout := args[0].(string)
 		model := args[1].(string)
@@ -317,6 +435,14 @@ func runPostInstallOperation(chroot bool, operation string, args []interface{}) 
 		if err != nil {
 			return err
 		}
+	/* !! ### grub-install
+	 * Installs GRUB to the specified partition.
+	 *
+	 * **Accepts**:
+	 * - BootDirectory (`string`): The path for the boot dir (usually `/boot`).
+	 * - InstallDevice (`string`): The disk where the boot partition is located.
+	 * - Target (`string`): The target firmware. Either `bios` for legacy systems or `efi` for UEFI systems.
+	 */
 	case "grub-install":
 		bootDirectory := args[0].(string)
 		installDevice := args[1].(string)
@@ -334,6 +460,9 @@ func runPostInstallOperation(chroot bool, operation string, args []interface{}) 
 		if err != nil {
 			return err
 		}
+	/* !! ### grub-default-config
+	 * TODO: Document
+	 */
 	case "grub-default-config":
 		currentConfig, err := GetGrubConfig(targetRoot)
 		if err != nil {
@@ -347,6 +476,9 @@ func runPostInstallOperation(chroot bool, operation string, args []interface{}) 
 		if err != nil {
 			return err
 		}
+	/* !! ### grub-add-script
+	 * TODO: Document
+	 */
 	case "grub-add-script":
 		for _, arg := range args {
 			scriptPath := arg.(string)
@@ -355,6 +487,9 @@ func runPostInstallOperation(chroot bool, operation string, args []interface{}) 
 				return err
 			}
 		}
+	/* !! ### grub-remove-script
+	 * TODO: Document
+	 */
 	case "grub-remove-script":
 		for _, arg := range args {
 			scriptName := arg.(string)
@@ -363,6 +498,9 @@ func runPostInstallOperation(chroot bool, operation string, args []interface{}) 
 				return err
 			}
 		}
+	/* !! ### grub-mkconfig
+	 * TODO: Document
+	 */
 	case "grub-mkconfig":
 		outputPath := args[0].(string)
 		err := RunGrubMkconfig(targetRoot, outputPath)

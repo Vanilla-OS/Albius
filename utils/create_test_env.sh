0764671ad93d64e0a85335433e3f27ac93879675
Mateus Melchiades <matbme@duck.com>
2023-11-04 19:01:48 +0000

    Yes

--- utils/create_test_env.sh
+++ utils/create_test_env.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-distrobox-create -r -I -ap "golang libbtrfs-dev libdevmapper-dev libgpgme-dev build-essential pkg-config lvm2 parted udev" -i ghcr.io/vanilla-os/vso:main albius_test
+distrobox-create -r -I -Y -ap "golang libbtrfs-dev libdevmapper-dev libgpgme-dev build-essential pkg-config lvm2 parted udev" -i ghcr.io/vanilla-os/vso:main albius_test
